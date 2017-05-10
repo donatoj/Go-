21ef117596874323028e7ea5e649e9f3a4197807
Jordan <toymachine84@yahoo.com>
2017-05-10 01:07:16 -0700

    fb login operational

--- Go!/ViewController.swift
+++ Go!/ViewController.swift
@@ -7,19 +7,40 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let loginButton = FBSDKLoginButton()
+        loginButton.center = view.center
+        loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+        view.addSubview(loginButton)
+        
+        loginButton.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        
+        print("user did log out of facebook")
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        
+        if error != nil {
+            print("login error \(error)")
+        } else {
+            print("login successful")
+        }
+    }
 
 }
 
