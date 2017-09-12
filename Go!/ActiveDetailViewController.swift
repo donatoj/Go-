398efff8a1d1da2453619e7a00d12dd25e6dca17
Jordan <toymachine84@yahoo.com>
2017-09-12 01:47:56 -0700

    implement active list detail view controller

---
+++ Go!/ActiveDetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ActiveDetailViewController.swift
+//  Go!
+//
+//  Created by Jordan Donato on 9/11/17.
+//  Copyright © 2017 Go!. All rights reserved.
+//
+
+import UIKit
+
+class ActiveDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onCompleteButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func onCancelPressed(_ sender: UIButton) {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
